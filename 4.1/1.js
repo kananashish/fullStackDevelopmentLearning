a7992eccd1bd3045ae8acb3db358c874330d32d0
kananashish <ashishkananconnect@gmail.com>
2024-10-20 15:42:58 +0530

    new commit

---
+++ 4.1/1.js
@@ -0,0 +1,10 @@
+//package.json is an important file for node.js
+//rust => carge.toml
+// const fs = require("fs");
+// fs.readFile("a.txt", function(err,data) {
+//     console.log(data);
+// })
+console.log("__dirname")
+//above line prints dirname as a string
+console.log(__dirname)
+//above line prints the directory of the file
