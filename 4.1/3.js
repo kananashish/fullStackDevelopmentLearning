cd6ee4653ababb79eba136d9c8eff37774e9d74b
kananashish <ashishkananconnect@gmail.com>
2024-11-14 12:34:23 +0530

    new commit

--- 4.1/3.js
+++ 4.1/3.js
@@ -1,4 +1,6 @@
 const path = require("path");
 
 console.log(__dirname)
+//above line prints the directory of the file
 console.log(path.join(__dirname, "index.js"))
+//above line prints the directory with index.js at the end (it is also included in the directory path)
