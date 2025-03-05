a5b8ec4664445e46b00f4de7f318182cd6c45ae9
kananashish <ashishkananconnect@gmail.com>
2025-03-05 14:15:46 +0530

    new files added

---
+++ CAC/30_Filter_map_and_reduce/1.js
@@ -0,0 +1,7 @@
+const coding = ['js','ruby', 'java', 'python', 'cpp'];
+//forEach() doesn't return any value
+const values = coding.forEach( (item) => {
+    console.log(item);
+});
+
+console.log(values);
