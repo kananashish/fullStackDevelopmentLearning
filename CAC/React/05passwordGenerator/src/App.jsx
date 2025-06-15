661090d7dff5f1261ac4c09c9d4ca07762aca5fb
kananashish <ashishkananconnect@gmail.com>
2025-06-15 10:12:56 +0530

    minor changes

--- CAC/React/05passwordGenerator/src/App.jsx
+++ CAC/React/05passwordGenerator/src/App.jsx
@@ -49,7 +49,10 @@ function App() {
           readOnly
           ref={passwordRef}
           />
-          <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <button 
+          onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+            Copy
+          </button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
