904b7ec1852bf31b5e39c7b58abe2fd7f56b1ddd
kananashish <ashishkananconnect@gmail.com>
2025-07-24 23:22:52 +0530

    react router

--- CAC/React/07reactRouter/src/components/Footer/Footer.jsx
+++ CAC/React/07reactRouter/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
     return (
         <footer className="bg-white border-y">
@@ -35,7 +36,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/kananshish"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -70,9 +71,9 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                        © 2025
+                        <a href="http://ashishkanan.netlify.app" className="hover:underline">
+                            ashishkanan
                         </a>
                         . All Rights Reserved.
                     </span>
